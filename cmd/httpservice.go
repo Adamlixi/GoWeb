523086757f81878a3cf46167a21c6af86272494f
xinan.li <xinan.li@shopee.com>
2020-08-26 20:09:04 +0800

    xxx

--- cmd/httpservice.go
+++ cmd/httpservice.go
@@ -1,5 +1,10 @@
 package main
 
+import "net/http"
+
 func main()  {
+	mux := http.NewServeMux()
+	file := http.FileServer(http.Dir("/public"))
+	mux.Handle("/static/",http.StripPrefix("/static/",file))
 	
 }
